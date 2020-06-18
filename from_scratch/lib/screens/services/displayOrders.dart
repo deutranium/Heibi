dd1127f20b55b932a0981c9ec3d756aed6ee5a03
aineshsan <ainesh.sannidhi@students.iiit.ac.in>
2020-06-19 01:41:01 +0530

    pahulyeet

--- from_scratch/lib/screens/services/displayOrders.dart
+++ from_scratch/lib/screens/services/displayOrders.dart
@@ -1,4 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blue,
+          accentColor: Colors.orange),
+      home: DisplayOrders(),
+    ));
+
+class DisplayOrders extends StatefulWidget {
+  @override
+  _DisplayOrdersState createState() => _DisplayOrdersState();
+}
+
+class _DisplayOrdersState extends State<DisplayOrders> {
+  String todoTitle = "";
+
+  createTodos() {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyTodos").document(todoTitle);
+
+    //Map
+    Map<String, String> todos = {"todoTitle": todoTitle};
+
+    documentReference.setData(todos).whenComplete(() {
+      print("$todoTitle created");
+    });
+  }
+
+  deleteTodos(item) {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyTodos").document(item);
+
+    documentReference.delete().whenComplete(() {
+      print("$item deleted");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Les Orders"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  title: Text("Add Shiz"),
+                  content: TextField(
+                    onChanged: (String value) {
+                      todoTitle = value;
+                    },
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          createTodos();
+
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("Add"))
+                  ],
+                );
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance.collection("MyTodos").snapshots(),
+          builder: (context, snapshots) {
+            if (snapshots.hasData) {
+              return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: snapshots.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot documentSnapshot =
+                        snapshots.data.documents[index];
+                    return Dismissible(
+                        onDismissed: (direction) {
+                          deleteTodos(documentSnapshot["todoTitle"]);
+                        },
+                        key: Key(documentSnapshot["todoTitle"]),
+                        child: Card(
+                          elevation: 4,
+                          margin: EdgeInsets.all(8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: ListTile(
+                            title: Text(documentSnapshot["todoTitle"]),
+                            trailing: IconButton(
+                                icon: Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                                onPressed: () {
+                                  deleteTodos(documentSnapshot["todoTitle"]);
+                                }),
+                          ),
+                        ));
+                  });
+            } else {
+              return Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
+
+
+
+
+
+/*import 'package:flutter/material.dart';
 
 class DisplayOrders extends StatefulWidget {
   @override
@@ -15,4 +141,4 @@ class _DisplayOrdersState extends State<DisplayOrders> {
       
     );
   }
-}
+}*/
