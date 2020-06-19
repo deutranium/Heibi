ea984a244350bab65993ca1cd09bb54665f537b3
deutranium <29124655+deutranium@users.noreply.github.com>
2020-06-19 07:39:05 +0530

    Added change password functionality

--- from_scratch/lib/screens/services/profile.dart
+++ from_scratch/lib/screens/services/profile.dart
@@ -90,6 +90,14 @@ class ProfileState extends State<ProfileScreen> {
     );
   }
 
+  // FirebaseUser user = await FirebaseAuth.instance.currentUser();
+  //   AuthResult authResult = await user.reauthenticateWithCredential(
+  //     EmailAuthProvider.getCredential(
+  //       email: user.email,
+  //       password: _oldPassword,
+  //     ),
+  //   );
+
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
@@ -115,17 +123,24 @@ class ProfileState extends State<ProfileScreen> {
 
                 _formKey.currentState.save();
                 final user = await _auth.getUser();
+                final mailID = user.email;
+                print(mailID);
+                dynamic result = await _auth.signInWithEmail(mailID, _oldPassword);
+                print("plz work");
 
-                user.updatePassword(_newPassword).then((_){
-                  print("Succesfull changed password");
-                }).catchError((error){
-                  print("Password can't be changed" + error.toString());
-                  //This might happen, when the wrong password is in, the user isn't found, or if the user hasn't logged in recently.
-                });
+                if(result != null){
+                  user.updatePassword(_newPassword).then((_){
+                    print("Succesfull changed password");
+                  }).catchError((error){
+                    print("Password can't be changed" + error.toString());
+                    //This might happen, when the wrong password is in, the user isn't found, or if the user hasn't logged in recently.
+                  });
 
-                // If the form is valid, display a Snackbar.
-                Scaffold.of(context)
-                    .showSnackBar(SnackBar(content: Text('Password Updated')));
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Password Updated')));
+                }
+                
               },
               child: Text('Submit'),
             ),
