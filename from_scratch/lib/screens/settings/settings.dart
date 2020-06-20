88619fcacbe3314040a6ca465b64dad231d22fe9
codelixir <pahulpreet.singh@research.iiit.ac.in>
2020-06-20 17:35:32 +0530

    made changes to home and settings page.

--- from_scratch/lib/screens/settings/settings.dart
+++ from_scratch/lib/screens/settings/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:from_scratch/models/option.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:from_scratch/shared/loading.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -8,13 +9,7 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  List<Option> options = [
-    Option('DLF', 'f0'),
-    Option('J.C.', 'f1'),
-    Option('Washing Machine', 'f2'),
-    Option('Bakul Warehouse', 'f3'),
-    Option('NBH Table Tennis', 'f4'),
-  ];
+  bool loading = true;
 
   Widget optionTemplate(item) {
     return CheckboxListTile(
@@ -29,76 +24,110 @@ class _SettingsScreenState extends State<SettingsScreen> {
   }
 
   @override
+  void initState() {
+    _getFavs();
+    print("yeetn't");
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[400],
-        title: Text('Personalise'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Favourites",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                RaisedButton(
-                  color: Colors.blueGrey[400],
-                  child: Text(
-                    'SAVE',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.blueGrey[100],
+            appBar: AppBar(
+              backgroundColor: Colors.blueGrey[400],
+              title: Text('Personalise'),
+            ),
+            body: Container(
+              margin: EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Favourites",
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      RaisedButton(
+                        color: Colors.blueGrey[400],
+                        child: Text(
+                          'SAVE',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        onPressed: () {
+                          _saveFavs();
+                          print('yeet');
+                        },
+                      ),
+                    ],
                   ),
-                  onPressed: () {
-                    print('yeet');
-                  },
-                ),
-              ],
-            ),
-            Column(
-                children: options.map((item) => optionTemplate(item)).toList()),
-            Spacer(),
-            Text(
-              "More Settings",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            RaisedButton(
-              color: Colors.pinkAccent[400],
-              child: Text(
-                'Reorder Navigation',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
+                  Column(
+                      children:
+                          options.map((item) => optionTemplate(item)).toList()),
+                  // Spacer(),
+                  // Text(
+                  //   "More Settings",
+                  //   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  // ),
+                  // RaisedButton(
+                  //   color: Colors.pinkAccent[400],
+                  //   child: Text(
+                  //     'Reorder Navigation',
+                  //     style: TextStyle(
+                  //       color: Colors.white,
+                  //       fontSize: 20,
+                  //     ),
+                  //   ),
+                  //   onPressed: () {
+                  //     print('Button 1');
+                  //   },
+                  // ),
+                  // RaisedButton(
+                  //   color: Colors.deepPurpleAccent[400],
+                  //   child: Text(
+                  //     'Modify User Details',
+                  //     style: TextStyle(
+                  //       color: Colors.white,
+                  //       fontSize: 20,
+                  //     ),
+                  //   ),
+                  //   onPressed: () {
+                  //     print('Button 2');
+                  //   },
+                  // ),
+                ],
               ),
-              onPressed: () {
-                print('Button 1');
-              },
             ),
-            RaisedButton(
-              color: Colors.deepPurpleAccent[400],
-              child: Text(
-                'Modify User Details',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              onPressed: () {
-                print('Button 2');
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+  }
+
+  Future _getFavs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    for (Option item in options) {
+      bool value = prefs.getBool(item.key);
+      if (value == true) {
+        item.state = true;
+      } else {
+        item.state = false;
+      }
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
+  void _saveFavs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    for (Option item in options) {
+      prefs.setBool(item.key, item.state);
+    }
   }
 }
