9eed9193a1ea513dc0ddbadb0898c2a27002efc7
ewan-mcjesus <saru.d.gr8@gmail.com>
2020-06-20 17:21:59 +0530

    fixed small stuff

--- from_scratch/lib/screens/auth/register.dart
+++ from_scratch/lib/screens/auth/register.dart
@@ -21,79 +21,92 @@ class _RegisterState extends State<Register> {
   bool loading = false;
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-        backgroundColor: Colors.deepPurple[100],
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurple[400],
-          title: Text('Register'),
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('login'),
-              onPressed: () {
-                widget.toggleView();
-              },
-            )
-          ],
-        ),
-        body: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Email'
-                      ),
-                      validator: (val) => val.isEmpty ? 'enter email' : null,
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      },
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'password'
-                      ),
-                      validator: (val) => val.length < 6 ? 'enter valid password(6+ chars)' : null,
-                      obscureText: true,
-                      onChanged: (val) {
-                        setState(() => passwd = val);
-                      },
-                    ),
-                    SizedBox(height: 20.0),
-                    RaisedButton(
-                      color: Colors.orange[100],
-                      child: Text('register'),
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          setState(() => loading = true);
-                          dynamic result = await _auth.regWithEmail(email, passwd);
-                          if (result == null){
-                            loading = false;
-                            setState( () => error = 'please give valid email & password');
-                         }print(email);
-                          print(passwd);
-                        }
-                      },
-                      // onPressed: () async {
-                      //   _auth.signOutGoogle();
-                      // },
-                    ),
-                    SignInButton(
-                      Buttons.GoogleDark,
-                      // backgroundColor: Colors.deepOrange,
-                      onPressed: () async {
-                        dynamic result = _auth.signInWithGoogle();
-                        print(result);
-                      },
-                      // child: Text('G'),
-                      ),
-                    Text(error)
-                  ],
-                ))));
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.deepPurple[100],
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple[400],
+              title: Text('Register'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('login'),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(hintText: 'Email'),
+                          validator: (val) =>
+                              val.isEmpty ? 'enter email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(hintText: 'password'),
+                          validator: (val) => val.length < 6
+                              ? 'enter valid password(6+ chars)'
+                              : null,
+                          obscureText: true,
+                          onChanged: (val) {
+                            setState(() => passwd = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        RaisedButton(
+                          color: Colors.orange[100],
+                          child: Text('register'),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result =
+                                  await _auth.regWithEmail(email, passwd);
+                              if (result == null) {
+                                loading = false;
+                                setState(() => error =
+                                    'please give valid email & password');
+                              }
+                              print(email);
+                              print(passwd);
+                            }
+                          },
+                          // onPressed: () async {
+                          //   _auth.signOutGoogle();
+                          // },
+                        ),
+                        SignInButton(
+                          Buttons.GoogleDark,
+                          // backgroundColor: Colors.deepOrange,
+
+                          onPressed: () async {
+                            setState(() {
+                              loading = true;
+                            });
+                            dynamic result = _auth.signInWithGoogle();
+                            if (result == null) {
+                              loading = false;
+                              setState(() =>
+                                  error = 'please give valid email & password');
+                            }
+                            print(result);
+                          },
+                          // child: Text('G'),
+                        ),
+                        Text(error)
+                      ],
+                    ))));
   }
 }
